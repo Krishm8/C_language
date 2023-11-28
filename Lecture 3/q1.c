d605e45936a5bf3dfa962041069a9756f08c28f6
Krishm8 <148517012+Krishm8@users.noreply.github.com>
2023-11-28 22:07:52 +0530

    Delete Lecture 3 directory

--- Lecture 3/q1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a;
-    float pi=3.14;
-    printf("Enter radius of circle =");
-    scanf("%d",&a);
-    printf("The area of a circle is :%0.2f",pi*a*a);
-    return 0;
-}
