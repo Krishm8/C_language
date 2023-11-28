dce64a0b11daf10686a0bf587c6a013146792595
Krishm8 <148517012+Krishm8@users.noreply.github.com>
2023-11-28 22:15:18 +0530

    Add files via upload

---
+++ Lecture 2.2/q2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the width =");
+    scanf("%d",&a);
+    printf("Enter the height =");
+    scanf("%d",&b);
+    printf("The area of a rectangle =%d",a*b);
+    return 0;
+}
---
+++ Lecture 2.2/q3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of base =");
+    scanf("%d",&a);
+    printf("Enter the value of height =");
+    scanf("%d",&b);
+    printf("The area of a triangle = %d",a*b/2);
+    return 0;
+}
